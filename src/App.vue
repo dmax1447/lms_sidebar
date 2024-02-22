098d29e64fed26e0b60e0103a6be045f40d8728f
Max Dragaytsev <40897499+dmax1447@users.noreply.github.com>
2024-02-22 12:57:45 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,130 @@
 <template>
   <div id="app">
+<!--    <nav>-->
+<!--      <ul>-->
+<!--        <li>-->
+<!--          <router-link to="/courses">Курсы</router-link>-->
+<!--          <ul>-->
+<!--            <li><router-link to="/courses/stat">Статистика</router-link></li>-->
+<!--            <ul>-->
+<!--              <li>-->
+<!--                <router-link-->
+<!--                    to="/courses/stat/assignment"-->
+<!--                    class="menu-link"-->
+<!--                    :class="{-->
+<!--                      disabled: menu.disabled.assignment,-->
+<!--                      hidden: menu.hidden.assignment-->
+<!--                    }"-->
+<!--                >Задания</router-link></li>-->
+<!--              <li><router-link to="/courses/stat/quiz">Тесты</router-link></li>-->
+<!--            </ul>-->
+
+<!--          </ul>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--          <router-link to="/tests">Тесты</router-link>-->
+<!--        </li>-->
+<!--        <li>-->
+<!--          <router-link to="/kit">Kit</router-link>-->
+<!--        </li>-->
+<!--      </ul>-->
+<!--    </nav>-->
+<!--    <hr>-->
     <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <ul>
+        <li v-for="rootLevel in nav" :key="rootLevel.id">
+          <router-link :to="rootLevel.path">{{rootLevel.title}}</router-link>
+          <ul v-if="rootLevel.children">
+            <li v-for="rootLevelChild in rootLevel.children" :key="rootLevelChild.id"><router-link to="/courses/stat">Статистика</router-link>
+              <ul v-if="rootLevelChild.children">
+                <li v-for="nestedLevelChild in rootLevelChild.children" :key="nestedLevelChild.id">
+                  <router-link
+                      :to="nestedLevelChild.path"
+                      class="menu-link"
+                      :class="{
+                      disabled: menu.disabled[nestedLevelChild.id],
+                      hidden: menu.hidden[nestedLevelChild.id]
+                    }"
+                  >{{ nestedLevelChild.title }}</router-link></li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
     </nav>
-    <router-view/>
   </div>
 </template>
+<script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  data() {
+    return {
+      menu: {
+        disabled: {},
+        hidden: {},
+      },
+      nav: [
+        {
+          path: '/courses', id: 'courses', title: 'Курсы',
+          children: [
+            {
+              path: '/courses/stat', id: 'courses|stat', title: 'Статистика',
+              children: [
+                {path: '/courses/stat/assignment', id: 'assignment', title: 'Задания',},
+                {path: '/courses/stat/quiz', id: 'courses|stat|tests', title: 'Тесты',}
+              ]
+            }
+          ]
+        },
+        {
+          path: '/tests', id: 'tests', title: 'Тесты'
+        },
+        {
+          path: '/kit', id: 'kit', title: 'Ui-kit'
+        }
+      ]
+
+
+
+    }
+  },
+
+  created() {
+    this.$root.eventBus.on('sidebar:hide_item', (e) => {
+      console.log('sidebar:hide_item', e)
+      if (!Object.hasOwn(this.menu.hidden, e.id)) {
+        this.$set(this.menu.hidden, e.id, true)
+        return
+      }
+      this.menu.hidden[e.id] = !this.menu.hidden[e.id]
+    })
+    this.$root.eventBus.on('sidebar:disable_item', (e) => {
+      if (!Object.hasOwn(this.menu.disabled, e.id)) {
+        this.$set(this.menu.disabled, e.id, true)
+        return
+      }
+      this.menu.disabled[e.id] = !this.menu.disabled[e.id]
+    })
+  }
 }
+</script>
+
+<style lang="scss" scoped>
+
 
 nav {
-  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  ul {
+    list-style: none;
+    padding-left: 16px;
+    margin-bottom: 20px;
+  }
+  li > ul {
+    margin-top: 10px;
+  }
 
   a {
     font-weight: bold;
@@ -27,6 +133,16 @@ nav {
     &.router-link-exact-active {
       color: #42b983;
     }
+    &.hidden {
+      color: red !important;
+      text-decoration: none;
+      display: none;
+    }
+    &.disabled {
+      color: lightgrey !important;
+      text-decoration: none;
+    }
+
   }
 }
 </style>
